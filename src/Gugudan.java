f93d996712c5683437a1d9c87ab0a97f5a02e26a
HJK9810 <ruby45342@gamail.com>
2020-08-19 15:30:05 +0900

    오류메시지 출력

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class Gugudan {
 
 	public static void main(String[] args) {
-		System.out.println("출력할 구구단은? : ");
+		System.out.println("Chose the number for 구구단? : ");
 		Scanner scanner = new Scanner(System.in);
 		int num = scanner.nextInt();
 		
 		if(num < 2 || num > 9) {
-			System.out.print("값을 잘못 입력했습니다.");
+			System.out.print("You put the wrong number.");
 		} else {
 			for(int i = 1 ; i<10 ; i++) {
 				System.out.println(num + " X " + i + " = " + num * i);
