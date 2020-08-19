25c9efb12f5519bfe9492b94153de93c222ddcaa
HJK9810 <ruby45342@gamail.com>
2020-08-19 15:14:55 +0900

    출력되는 한글 간소화

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Gugudan {
 
 	public static void main(String[] args) {
-		System.out.println("구구단중 출력할 단을 선택 : ");
+		System.out.println("출력할 구구단은? : ");
 		Scanner scanner = new Scanner(System.in);
 		int num = scanner.nextInt();
 		for(int i = 1 ; i<10 ; i++) {
