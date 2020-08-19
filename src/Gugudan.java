432b10ba16b7e9846693e1daf9e41583cf74fc04
HJK9810 <ruby45342@gamail.com>
2020-08-19 15:19:35 +0900

    오류메세지 추가

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -6,8 +6,13 @@ public class Gugudan {
 		System.out.println("출력할 구구단은? : ");
 		Scanner scanner = new Scanner(System.in);
 		int num = scanner.nextInt();
-		for(int i = 1 ; i<10 ; i++) {
-			System.out.println(num + " X " + i + " = " + num * i);
+		
+		if(num < 2 || num > 9) {
+			System.out.print("값을 잘못 입력했습니다.");
+		} else {
+			for(int i = 1 ; i<10 ; i++) {
+				System.out.println(num + " X " + i + " = " + num * i);
+			}
 		}
 	}
 
